feb04ac8d0f6709c4c4a9a715d142fa66c81eee1
Zack Bumpous <zackbumpous97@gmail.com>
2024-01-24 20:58:21 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace A1_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print Random Number section of code
+            Console.WriteLine("Printing random number. Intense computation incoming!");
+            Random rnd = new Random();
+            int randNum = rnd.Next(1, 11);
+            Console.WriteLine($"Random number: {randNum}");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            Console.WriteLine();
+        }
+    }
+}
