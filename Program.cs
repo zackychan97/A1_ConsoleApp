70d5453e880fcac14966e4a7b651a31f09180950
Zack Bumpous <zackbumpous97@gmail.com>
2024-01-25 19:31:22 -0600

    Refactors code to be in methods. Also adds some code/logic to the PrintRandomDino section of code and adds a new section/method skeleton for printing the random string alteration.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace A1_ConsoleApp
+﻿using System;
+
+namespace A1_ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             // Print Random Number section of code
+            PrintRandomNumber();
+            Console.WriteLine();
+            // Todays Date section of code
+            PrintTodaysDate();
+            Console.WriteLine();
+            // List of dinos section of code
+            PrintRandomDino();
+            Console.WriteLine();
+
+        }
+
+        public static void PrintRandomNumber()
+        {
             Console.WriteLine("Printing random number. Intense computation incoming!");
             Random rnd = new Random();
             int randNum = rnd.Next(1, 11);
@@ -12,12 +27,20 @@
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
             Console.WriteLine();
+        }
 
-            // Todays Date section of code
+        public static void PrintTodaysDate()
+        {
             string today = DateTime.Now.ToShortDateString();
             Console.WriteLine($"Todays date is: {today}");
+        }
 
-            // List of dinos section of code
+        public static void PrintRandomDino()
+        {
+            Console.WriteLine("Printing random dino.");
+            Random rnd = new Random();
+            int randNum = rnd.Next(0, 10);
+
             List<string> Dinosaurs = new List<string>();
             Dinosaurs.Add("Tyrannosaurus Rex");
             Dinosaurs.Add("Velociraptor");
@@ -29,6 +52,14 @@
             Dinosaurs.Add("Dilophosaurus");
             Dinosaurs.Add("Argentinosaurus");
             Dinosaurs.Add("Parasaurolophus");
+
+            var SortedDinosaursList = Dinosaurs.OrderBy(dino => dino).ToList();
+            Console.WriteLine(SortedDinosaursList[randNum]);
+        }
+
+        public static void PrintRandomStringAlteration(string str)
+        {
+            Console.WriteLine($"Altering your string: {str}");
 
         }
     }
